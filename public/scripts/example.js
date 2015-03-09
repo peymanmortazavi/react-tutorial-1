f4bcbb61468d1b750d5b779bcd6c41a7bde78096
Nir Boneh <nboneh5921@gmail.com>
2015-03-09 17:24:33 -0600

    compeleted milestone 1

--- public/scripts/example.js
+++ public/scripts/example.js
@@ -1,12 +1,53 @@
+var CommentForm = React.createClass({
+  render: function() {
+    return (
+      <form className="commentForm">
+        <input type="text" placeholder="Your name" />
+        <input type="text" placeholder="Say something..." />
+        <input type="submit" value="Post" />
+      </form>
+    );
+  }
+});
+
 var CommentBox = React.createClass({
   render: function() {
     return (
       <div className="commentBox">
-        Hello, world! I am a CommentBox.
+        <h1>Comments</h1>
+        <CommentList />
+   		<CommentForm />
       </div>
     );
   }
 });
+
+var CommentList = React.createClass({
+  render: function() {
+    return (
+      <div className="commentList">
+        <Comment author="Pete Hunt">This is one comment</Comment>
+        <Comment author="Jordan Walke">This is *another* comment</Comment>
+      </div>
+    );
+  }
+});
+
+var converter = new Showdown.converter();
+var Comment = React.createClass({
+  render: function() {
+  	var rawMarkup = converter.makeHtml(this.props.children.toString());
+    return (
+      <div className="comment">
+        <h2 className="commentAuthor">
+          {this.props.author}
+        </h2>
+        <span dangerouslySetInnerHTML={{__html: rawMarkup}} />
+      </div>
+    );
+  }
+});
+
 React.render(
   <CommentBox />,
   document.getElementById('content')
