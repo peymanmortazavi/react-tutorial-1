587e9cb7199c42c7a5d86e21e3fc52e704f07928
mazdeh <vahid.mazdeh@gmail.com>
2015-03-09 17:23:27 -0600

    completed milestone 1

--- public/scripts/example.js
+++ public/scripts/example.js
@@ -41,8 +41,8 @@ var Comment = React.createClass({
 				<h2 className="commentAuthor">
 					{this.props.author}
 				</h2>
-				{converter.makeHtml(this.props.children.toString())}
-				// <span dangerouslySetInnerHTML={{__html: rawMarkup}} />
+				// {converter.makeHtml(this.props.children.toString())}
+				<span dangerouslySetInnerHTML={{__html: rawMarkup}} />
 			</div>
 			);
 	}
