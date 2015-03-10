f12b9d435372f592152ccdb866c74d0336ee0dec
Nir Boneh <nboneh5921@gmail.com>
2015-03-09 18:01:50 -0600

    compeleted milestone 4

--- public/scripts/example.js
+++ public/scripts/example.js
@@ -38,6 +38,9 @@ var CommentBox = React.createClass({
     return {data: []};
   },
   handleCommentSubmit: function(comment) {
+    var comments = this.state.data;
+    var newComments = comments.concat([comment]);
+    this.setState({data: newComments});
     $.ajax({
       url: this.props.url,
       dataType: 'json',
